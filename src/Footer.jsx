cf387b09ccb3a8cfabb9bb836ae73799839e7b01
abir_mehmed <abirmehmed@gmail.com>
2024-01-06 20:16:51 +0600

    commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,22 +1,11 @@
 import React from "react";
 import "./Footer.css";
 import GitHubIcon from "./image/GitHub.png";
-import CodeSandboxIcon from "./image/CodeSandbox.png";
 
 const Footer = () => {
   return (
-    
     <footer className="footer">
-      <section id="credit">
-      <div className="members">
-        <p>Credits:</p>
-        <ul>
-          <li>Islam Md Abirul - ID no: 20170940</li>
-          <li>Jabborov Sherzod - ID no: 20170910</li>
-          <li>Nosirov Jamoliddin - ID no: 20170936</li>
-        </ul>
-      </div>
-      <div className="icons">
+      <div className="github">
         <a
           href="https://github.com/abirmehmed/Rapid-Analyzer"
           target="_blank"
@@ -24,21 +13,17 @@ const Footer = () => {
         >
           <img src={GitHubIcon} alt="GitHub" className="github-icon" />
         </a>
-        <a
-          href="https://codesandbox.io/p/sandbox/broken-mountain-sgg27t-sgg27t?file=%2Fsrc%2FSentimentAnalysis.js%3A17%2C10"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src={CodeSandboxIcon}
-            alt="CodeSandbox"
-            className="codesandbox-icon"
-          />
-        </a>
       </div>
-      </section>
+      <div className="credit">
+        <p>Credits:</p>
+        <ul>
+          <li>Islam Md Abirul - ID no: 20170940</li>
+          <li>Jabborov Sherzod - ID no: 20170910</li>
+          <li>Nosirov Jamoliddin - ID no: 20170936</li>
+          <li>Takhirov Saydullo - ID no: 20170908</li>
+        </ul>
+      </div>
     </footer>
-    
   );
 };
 
